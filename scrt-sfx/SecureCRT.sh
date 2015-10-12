41989b510d5944556ffa3725ab29cba48287ef52
maz-1 <ohmygod19993@gmail.com>
2015-10-12 16:42:15 +0800

    fix

--- scrt-sfx/SecureCRT.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-BINDIR=/usr/lib/scrt-sfx
-[ -f ~/.vandyke/SecureCRT/Config/SecureCRT.lic ] && exec $BINDIR/SecureCRT "$@" || (  install -Dm 644 /usr/share/vandyke/data/SecureCRT.lic ~/.vandyke/SecureCRT/Config/SecureCRT.lic && exec $BINDIR/SecureCRT "$@" )
--- scrt-sfx/SecureFX.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-BINDIR=/usr/lib/scrt-sfx
-[ -f ~/.vandyke/SecureCRT/Config/SecureFX.lic ] && exec $BINDIR/SecureFX "$@" ||  ( install -Dm 644 /usr/share/vandyke/data/SecureFX.lic ~/.vandyke/SecureCRT/Config/SecureFX.lic && exec $BINDIR/SecureFX "$@" )
